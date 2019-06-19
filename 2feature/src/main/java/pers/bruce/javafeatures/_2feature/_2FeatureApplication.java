6646b0712d07a5ed721a8ff88e1bdca723589fe3
iamtkjxlh <iamtkjxlh@163.com>
2019-06-19 09:57:09 +0800

    追加文件

---
+++ 2feature/src/main/java/pers/bruce/javafeatures/_2feature/_2FeatureApplication.java
@@ -0,0 +1,13 @@
+package pers.bruce.javafeatures._2feature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class _2FeatureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(_2FeatureApplication.class, args);
+    }
+
+}
