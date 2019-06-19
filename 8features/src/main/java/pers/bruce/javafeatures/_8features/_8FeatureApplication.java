186b280ed3995d67ab45b7ec871db7ed1a234aa6
iamtkjxlh <iamtkjxlh@163.com>
2019-06-19 10:35:35 +0800

    追加文件

---
+++ 8features/src/main/java/pers/bruce/javafeatures/_8features/_8FeatureApplication.java
@@ -0,0 +1,13 @@
+package pers.bruce.javafeatures._8features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class _8FeatureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(_8FeatureApplication.class, args);
+    }
+
+}
